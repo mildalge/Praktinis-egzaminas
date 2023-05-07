4debbb2fd9d7745811f7ff9184e99a86ea8117c7
mildalge <milda.gureckiene@gmail.com>
2023-05-07 07:43:37 +0300

    sunday

--- script.js
+++ script.js
@@ -44,3 +44,27 @@ window.addEventListener("scroll", function(){
         }
     }
 })
+
+
+// swiper
+const swiper = new Swiper('.swiper', {
+    // Optional parameters
+    direction: 'horizontal',
+    loop: true,
+  
+    // If we need pagination
+    pagination: {
+      el: '.swiper-pagination',
+    },
+  
+    // Navigation arrows
+    navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
+    },
+
+    // breakpoints:{
+    //     600:{
+    //         slidesPerView:1
+    // }
+  });
