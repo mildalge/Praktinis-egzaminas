33da3043b8491de3444ec03c0703d0d87563c056
mildalge <milda.gureckiene@gmail.com>
2023-05-08 18:27:35 +0300

    monday5

--- script.js
+++ script.js
@@ -51,25 +51,27 @@ const tabs = document.getElementsByClassName("tab")
 // const h1 = document.querySelector("h1")
 const buttons = document.getElementsByTagName("button")
 
-for (let i = 0; i < tabs.length; i++) {
-    // Paslepia visus tabus pradzioje
-    tabs[i].style.display = "none"
+// for (let i = 0; i < tabs.length; i++) {
+//     // Paslepia visus tabus pradzioje
+//     tabs[i].style.display = "none"
   
-}
+// }
 
 tabs[0].style.display = "block"
 
 
-function showTab(_event, id) {
+function showTab(event, id) {
     
     for (let i = 0; i < tabs.length; i++) {
         tabs[i].style.display = "none"
-        // buttons[i].classList.remove("selected")
+        buttons[2].classList.remove("selected")
+        buttons[3].classList.remove("selected")
+        buttons[4].classList.remove("selected")
     }
     
     document.getElementById(id).style.display = "block"
     
-    _event.currentTarget.classList.add("selected")
+    event.currentTarget.classList.add("selected")
 }
 
 
